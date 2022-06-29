9b89a20a714b0f0cf6f9fa2b0fd1c026e1791e04
Ricardo Guerra <RikkuG491@gmail.com>
2022-06-29 10:56:51 -0400

    commit tree ec4ad4c5351990706859a3460c3b49a0ce1256b0

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener("click", function() {
+        alert("You clicked the button");
+    })
 }
+
+/*const button = document.getElementById("button");
+console.log(button);
+
+const clickButton = () => {
+    alert("You clicked the button. Yes you did.")
+};
+
+button.addEventListener("click", clickButton)*/
